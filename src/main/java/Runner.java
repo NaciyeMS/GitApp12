7bb13bc3f8cbc526f9e7a0ef87a1c7ec9496ffad
NaciyeMS <nmacit.015@gmail.com>
2024-05-24 23:33:49 +0300

    yeniv1

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -3,7 +3,5 @@ public class Runner {
         //version 1
         System.out.println("hello git");
         System.out.println("git add. staging areaya alır");
-                //version 2
-        System.out.println("git commit -m\"\" ile versiyon oluşturulur");
     }
 }

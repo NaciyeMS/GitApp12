ec842b31381e7afebefb3fd30d4a5fa36d3792ba
NaciyeMS <nmacit.015@gmail.com>
2024-05-25 19:08:18 +0300

    v2

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -3,5 +3,7 @@ public class Runner {
         //version 1
         System.out.println("hello git");
         System.out.println("yeniden deniyorum");
+        //versiyon 2
+        System.out.println("git commmit -m ile ");
     }
 }
